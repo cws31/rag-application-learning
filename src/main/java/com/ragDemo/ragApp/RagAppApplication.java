9e90f618cfa00a33c9f6ab445197012a8e28e83b
cs-satyam <satyamwork4464@gmail.com>
2026-06-20 12:21:56 +0530

    pull request

--- src/main/java/com/ragDemo/ragApp/RagAppApplication.java
+++ src/main/java/com/ragDemo/ragApp/RagAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RagAppApplication {
-
         public static void main(String[] args) {
-
                 SpringApplication.run(
                                 RagAppApplication.class,
                                 args);
